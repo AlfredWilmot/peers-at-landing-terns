0f8b6aead14d98a85fd8da81a2853e85868092ac
Alfred Wilmot <alfred.wilmot@medtronic.com>
2022-10-20 08:20:21 +0100

    class descriptions

--- creational_patterns/factory/parsing_human_readable_files/main.py
+++ creational_patterns/factory/parsing_human_readable_files/main.py
@@ -3,6 +3,8 @@ import xml.etree.ElementTree as etree
 
 
 class JSONDataExtractor:
+    """ Extracts and parses data from *.json files """
+
     def __init__(self,filepath):
         self.data = dict()
         with openfile(filepath, mode='r', encoding='utf-8') as f:
@@ -13,9 +15,12 @@ class JSONDataExtractor:
         return self.data
 
 class XMLDataExtractor:
+    """ Extracts and parses data from *.xml files """
+
     def __init__(self,filepath):
         self.tree = etree.parse(filepath)
 
     @property
     def parsed_data(self):
         return self.tree
+
